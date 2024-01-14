d4068a4960e0d9e6bf18ae20baa610eaa41e3ca6
Onur Sengir <129227744+sengir92@users.noreply.github.com>
2024-01-14 12:49:07 +0300

    SQL BASICS - 2

---
+++ SQL BASICS-2/Homework 7.sql
@@ -0,0 +1,4 @@
+SELECT rating FROM film GROUP BY rating;
+SELECT replacement_cost,COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50;
+SELECT COUNT(*) FROM customer GROUP BY store_id;
+SELECT country_id,COUNT(city) FROM city GROUP BY country_id ORDER BY COUNT(city) DESC LIMIT 1;
