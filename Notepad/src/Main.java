d68098a4cb60523cb919a9c6772908c5c277db5e
sengir92 <onursengir92@gmail.com>
2024-01-22 22:33:15 +0300

    LF to CRLF conversion

---
+++ Notepad/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Define new object for get data from user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = input.nextLine();
+
+
+        try {
+            FileWriter fileWriter = new FileWriter("notepad.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.print(text);
+            printWriter.close();
+            fileWriter.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        try {
+            FileReader fileReader = new FileReader("notepad.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String readText = bufferedReader.readLine();
+            bufferedReader.close();
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
